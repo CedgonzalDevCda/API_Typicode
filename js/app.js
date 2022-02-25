8cf1fdb6387537ca726e67abcfb6e0c62715e56f
CedgonzalDevCda <98330296+CedgonzalDevCda@users.noreply.github.com>
2022-02-25 11:47:45 +0100

    add select user from userList

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
+//Component UserData
+let UserData = {
+    template: `<div>    
+        <h2>{{user.name}}</h2>
+            <p></p>
+        </div>`
+}
 // Vuejs - Root-Element
 let vm = new Vue({
     el: '#app',
     data: {
         users: []
     },
+    components: {
+        'user-data' : UserData,
+    },
     created: function (){
         this.loadUsers()
     },
     methods: {
+        //Load users from API
         loadUsers: async function () {
             try {
             let response = await fetch('https://jsonplaceholder.typicode.com/users')
