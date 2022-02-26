044d716a7e508a5e25560167dd782996a4250648
CedgonzalDevCda <98330296+CedgonzalDevCda@users.noreply.github.com>
2022-02-26 15:30:11 +0100

    add Load user from user and Userdata

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 //Component UserData
 let UserData = {
-    template: `<div>    
-        <h2>{{user.name}}</h2>
-            <p></p>
-        </div>`
+    template: `
+        <div>    
+            <h2>{{user.name}} ({{ user.username }})</h2>
+            <p>Id n°{{ user.id }}</p>
+            <ul>
+                <li>e-mail: {{ user.email }}</li>
+                <li>tel: {{ user.phone }}</li>
+                <li>web: {{ user.website }}</li>
+            </ul>
+            <p><i>Adresse: {{ user.address.street }}, {{ user.address.suite }} {{ user.address.zipcode }}</i></p>
+            <p>Entreprise: {{ user.company.name }}</p>
+        </div>`,
+    props: ['user']
 }
+
 // Vuejs - Root-Element
 let vm = new Vue({
     el: '#app',
     data: {
+        user:'',
+        userObj:'',
+        userName: '',
+        userId: '',
         users: []
     },
     components: {
@@ -17,6 +31,7 @@ let vm = new Vue({
     created: function (){
         this.loadUsers()
     },
+    
     methods: {
         //Load users from API
         loadUsers: async function () {
@@ -28,6 +43,19 @@ let vm = new Vue({
             } catch(e) {
             console.error('ERREUR', e)
             }
+        },
+        findUser: function() {
+            this.userObj = this.users.find((user) => user.name == this.userName)
+            this.userId = this.userObj.id
+            this.loadUser()
+            this.userObj = null
+        },
+        // Load user selected from users
+        loadUser: async function () {
+            let response = await fetch('https://jsonplaceholder.typicode.com/users/' + this.userId)
+            let user = await 
+            response.json()
+            this.user = user 
         }
     }
 });
